8bc4148551b6d389e291f12e5f48f4c726502d43
ArjDev69 <bruno96araujo@gmail.com>
2022-05-02 15:03:43 -0300

    update webservices and models

---
+++ CoffeeApp/CoffeeApp/Models/Order.swift
@@ -0,0 +1,30 @@
+//
+//  Order.swift
+//  CoffeeApp
+//
+//  Created by bruno araujo on 22/04/22.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+    case cappuccino
+    case latte
+    case espressino
+    case cortado
+}
+
+enum CoffeeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable {
+    
+    let name: String
+    let email: String
+    let type: CoffeeType
+    let size: CoffeeSize
+    
+}
