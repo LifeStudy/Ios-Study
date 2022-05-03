3c437bd61749073f181b319b2a224057d580765f
Bruno Araujo <arjsilva@brq.com>
2022-05-03 19:06:11 -0300

    feat: add list type coffee

--- CoffeeApp/CoffeeApp/Controllers/AddOrderViewController.swift
+++ CoffeeApp/CoffeeApp/Controllers/AddOrderViewController.swift
@@ -8,6 +8,25 @@
 import Foundation
 import UIKit
 
-class AddOrderViewController: UIViewController {
+class AddOrderViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private var vm = AddCoffeeOrderViewModel()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.vm.types.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CoffeeTypeCell", for: indexPath)
+        
+        cell.textLabel?.text = self.vm.types[indexPath.row]
+        return cell
+    }
     
 }
---
+++ CoffeeApp/CoffeeApp/View Models/AddCoffeeOrderViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  AddCoffeeOrderViewModel.swift
+//  CoffeeApp
+//
+//  Created by user on 02/05/22.
+//
+
+import Foundation
+
+struct AddCoffeeOrderViewModel {
+    
+    var name: String?
+    var email: String?
+    
+    var types: [String] {
+        return CoffeeType.allCases.map { $0.rawValue.capitalized }
+    }
+    
+    var sizes: [String] {
+        return CoffeeType.allCases.map { $0.rawValue.capitalized }
+    }
+    
+}
