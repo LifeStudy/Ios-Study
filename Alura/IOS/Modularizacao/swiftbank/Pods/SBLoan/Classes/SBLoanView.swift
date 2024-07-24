7d1bb90a982b038e4752efabf5c9145c40bb94d7
ArjJobis <bruno96araujo@gmail.com>
2024-07-24 19:57:08 -0300

    renma folder

---
+++ Alura/IOS/Modularizacao/swiftbank/Pods/SBLoan/Classes/SBLoanView.swift
@@ -0,0 +1,18 @@
+//
+//  SBLoanView.swift
+//  Pods
+//
+//  Created by bruno araujo on 22/07/24.
+//
+
+import SwiftUI
+
+struct SBLoanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SBLoanView()
+}
